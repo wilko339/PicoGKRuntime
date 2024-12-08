fc543b446da0dea2542c06301fb64cd68d189e80
Toby Wilkinson <toby.wilkinson339@gmail.com>
2024-12-09 00:38:31 +0100

    Misc + openmp

--- API/PicoGKBuild.h
+++ API/PicoGKBuild.h
@@ -1,7 +1,7 @@
 #ifndef PICOGK_BUILD_H
 #define PICOGK_BUILD_H
 
-#define PICOGK_BUILD       "2024-05-22 23:56:31"
+#define PICOGK_BUILD       "2024-12-08 23:31:34"
 #define PICOGK_LIB_VERSION "1.5.0"
 #define PICOGK_LIB_NAME    "picogkgh.1.5"
 
